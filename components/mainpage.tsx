afbddd9965d52612be3f8654d60de6a9c830115f
AvanFabian <avanfabiandaniswara@gmail.com>
2023-11-01 08:55:15 +0700

    update-dummy mainpage structure

--- components/mainpage.tsx
+++ components/mainpage.tsx
@@ -8,17 +8,16 @@ const mainpage = () => {
       <div className="w-full bg-gradient-to-l from-[#bc998e] to-[#8B6B61] mt-6 py-6 px-4 rounded-2xl">
         <Grid columns="2" gap="2" width="auto">
           <div className="flex flex-col align-middle my-auto">
-            <h1 className="text-[#fffdf1] text-2xl font-bold mb-4 capitalize subpixel-antialiased">Matematika bagian 1</h1>
-            <h2 className="text-[#fffdf1] text-xl font-normal subpixel-antialiased">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+            <h1 className="text-[#fffdf1] text-2xl font-bold mb-4 capitalize subpixel-antialiased">
+              Matematika bagian 1
+            </h1>
+            <h2 className="text-[#fffdf1] text-xl font-normal subpixel-antialiased">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            </h2>
           </div>
           <div className="flex justify-end items-center">
             <div className="flex flex-row w-max gap-4 cursor-pointer items-center px-4 py-2 h-max bg-transparent border-[3px] border-b-[5px] hover:opacity-75 border-[#49494962] rounded-2xl">
-                <Image 
-                  src={panduan}
-                  width={55}
-                  height={32}
-                  alt="WISE Edu Logo"
-                />
+              <Image src={panduan} width={55} height={32} alt="WISE Edu Logo" />
               <h2 className="text-[#ffffff] text-lg font-medium subpixel-antialiased capitalize">
                 Panduan Belajar
               </h2>
@@ -26,7 +25,39 @@ const mainpage = () => {
           </div>
         </Grid>
       </div>
-      <Grid columns="3" className="mt-16">
+      <div className="flex flex-col mt-16 gap-32">
+        <div className="flex flex-row">
+        <button className="flex transition-all rounded-full m-auto w-max py-8 px-10 h-max bg-[#e49937]">
+          1
+        </button>
+        <button className="flex transition-all rounded-full m-auto w-max py-8 px-10 h-max bg-[#e49937]">
+          1
+        </button>
+        <button className="flex transition-all rounded-full m-auto w-max py-8 px-10 h-max bg-[#e49937]">
+          1
+        </button>
+        </div>
+        <div className="flex flex-row">
+        <button className="flex transition-all rounded-full m-auto w-max py-8 px-10 h-max bg-[#e49937]">
+          1
+        </button>
+        <button className="flex transition-all rounded-full m-auto w-max py-8 px-10 h-max bg-[#e49937]">
+          1
+        </button>
+        </div>
+        <div className="flex flex-row">
+        <button className="flex transition-all rounded-full m-auto w-max py-8 px-10 h-max bg-[#e49937]">
+          1
+        </button>
+        <button className="flex transition-all rounded-full m-auto w-max py-8 px-10 h-max bg-[#e49937]">
+          1
+        </button>
+        <button className="flex transition-all rounded-full m-auto w-max py-8 px-10 h-max bg-[#e49937]">
+          1
+        </button>
+        </div>
+      </div>
+      {/* <Grid columns="3" className="mt-16">
         <div className="justify-self-center">
         <button className="flex transition-all rounded-full w-max py-7 px-10 h-max bg-[#2b77b4]">
             1
@@ -76,7 +107,7 @@ const mainpage = () => {
             3
         </button>
         </div>
-      </Grid>
+      </Grid> */}
     </div>
   );
 };
