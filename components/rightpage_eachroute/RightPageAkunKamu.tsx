8095f3eaf7a3a9fe386326606d2906a3730384bb
AvanFabian <avanfabiandaniswara@gmail.com>
2023-11-16 14:11:56 +0700

    marginbottom-rightpageakun

--- components/rightpage_eachroute/RightPageAkunKamu.tsx
+++ components/rightpage_eachroute/RightPageAkunKamu.tsx
@@ -49,7 +49,7 @@ const Rightpage_home = () => {
       </div>
       <div className="flex flex-col w-full mt-8 ml-3">
         <div className="flex relative flex-col w-full h-max mt-4 bg-[#ebe2cd] border-[3px] border-[#c4bfbf62] rounded-2xl">
-          <div className="ml-1 mt-1 px-3 py-3">
+          <div className="ml-1 mt-1 mb-1 px-3 py-3">
             <div className="flex flex-row mr-1 ">
               <h2 className="text-[#414040] text-sm font-bold subpixel-antialiased capitalize">
                 Pengaturan Akun Kamu < br/> dapat dilakukan di sini
