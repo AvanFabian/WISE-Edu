1e5093fbba3ddb51a0c4bcf6c17f4b4271c4270c
AvanFabian <avanfabiandaniswara@gmail.com>
2023-11-17 08:07:42 +0700

    mapelmenu semi-fixed

--- components/RightPageEachRoute/PelajaranMenu.tsx
+++ components/RightPageEachRoute/PelajaranMenu.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import * as HoverCard from "@radix-ui/react-hover-card";
 import Image from "next/image";
 
-const PelajaranMenu = ({ mapel }) => (
+const HoveringCard = ({ mapel }) => (
   console.log(mapel),
   (
     <HoverCard.Root>
@@ -15,160 +15,217 @@ const PelajaranMenu = ({ mapel }) => (
           className="cursor-pointer"
         />
       </HoverCard.Trigger>
-      <HoverCard.Portal>
+      <HoverCard.Portal className="h-[500px] overflow-scroll">
         <HoverCard.Content
-          className="overflow-y-scroll data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade w-[300px] rounded-xl bg-[#ebe2cd] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:transition-all"
+          className="data-[side=bottom]:animate-slideUpAndFade data-[side=right]:animate-slideLeftAndFade data-[side=left]:animate-slideRightAndFade data-[side=top]:animate-slideDownAndFade w-[300px] rounded-xl bg-[#ebe2cd] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] data-[state=open]:transition-all "
           sideOffset={5}
         >
-          <div className="flex flex-col">
-            <div className="flex border-b-2 border-[#b9b7b79f] ">
-              <span className="text-xs ml-4 py-2 text-center text-[#414040] font-bold uppercase">
-                Mata Pelajaran
-              </span>
+          <div className="flex flex-row">
+            <div className="flex flex-col">
+              <div className="flex border-b-2 border-[#b9b7b79f] ">
+                <span className="text-xs ml-4 py-2 text-center text-[#414040] font-bold uppercase">
+                  Mata Pelajaran
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
             </div>
-            <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
-              <Image
-                src={mapel[0].icon}
-                width={30}
-                height={30}
-                alt="Radix UI"
-                className="ml-4 cursor-pointer"
-              />
-              <span className="text-xs text-[#414040] font-medium ml-2">
-                {mapel[0].mapel}
-              </span>
-            </div>
-            <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
-              <Image
-                src={mapel[0].icon}
-                width={30}
-                height={30}
-                alt="Radix UI"
-                className="ml-4 cursor-pointer"
-              />
-              <span className="text-xs text-[#414040] font-medium ml-2">
-                {mapel[0].mapel}
-              </span>
-            </div>
-            <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
-              <Image
-                src={mapel[0].icon}
-                width={30}
-                height={30}
-                alt="Radix UI"
-                className="ml-4 cursor-pointer"
-              />
-              <span className="text-xs text-[#414040] font-medium ml-2">
-                {mapel[0].mapel}
-              </span>
-            </div>
-            <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
-              <Image
-                src={mapel[0].icon}
-                width={30}
-                height={30}
-                alt="Radix UI"
-                className="ml-4 cursor-pointer"
-              />
-              <span className="text-xs text-[#414040] font-medium ml-2">
-                {mapel[0].mapel}
-              </span>
-            </div>
-            <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
-              <Image
-                src={mapel[0].icon}
-                width={30}
-                height={30}
-                alt="Radix UI"
-                className="ml-4 cursor-pointer"
-              />
-              <span className="text-xs text-[#414040] font-medium ml-2">
-                {mapel[0].mapel}
-              </span>
-            </div>
-            <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
-              <Image
-                src={mapel[0].icon}
-                width={30}
-                height={30}
-                alt="Radix UI"
-                className="ml-4 cursor-pointer"
-              />
-              <span className="text-xs text-[#414040] font-medium ml-2">
-                {mapel[0].mapel}
-              </span>
-            </div>
-            <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
-              <Image
-                src={mapel[0].icon}
-                width={30}
-                height={30}
-                alt="Radix UI"
-                className="ml-4 cursor-pointer"
-              />
-              <span className="text-xs text-[#414040] font-medium ml-2">
-                {mapel[0].mapel}
-              </span>
-            </div>
-            <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
-              <Image
-                src={mapel[0].icon}
-                width={30}
-                height={30}
-                alt="Radix UI"
-                className="ml-4 cursor-pointer"
-              />
-              <span className="text-xs text-[#414040] font-medium ml-2">
-                {mapel[0].mapel}
-              </span>
-            </div>
-            <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
-              <Image
-                src={mapel[0].icon}
-                width={30}
-                height={30}
-                alt="Radix UI"
-                className="ml-4 cursor-pointer"
-              />
-              <span className="text-xs text-[#414040] font-medium ml-2">
-                {mapel[0].mapel}
-              </span>
-            </div>
-            <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
-              <Image
-                src={mapel[0].icon}
-                width={30}
-                height={30}
-                alt="Radix UI"
-                className="ml-4 cursor-pointer"
-              />
-              <span className="text-xs text-[#414040] font-medium ml-2">
-                {mapel[0].mapel}
-              </span>
-            </div>
-            <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
-              <Image
-                src={mapel[0].icon}
-                width={30}
-                height={30}
-                alt="Radix UI"
-                className="ml-4 cursor-pointer"
-              />
-              <span className="text-xs text-[#414040] font-medium ml-2">
-                {mapel[0].mapel}
-              </span>
-            </div>
-            <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
-              <Image
-                src={mapel[0].icon}
-                width={30}
-                height={30}
-                alt="Radix UI"
-                className="ml-4 cursor-pointer"
-              />
-              <span className="text-xs text-[#414040] font-medium ml-2">
-                {mapel[0].mapel}
-              </span>
+            <div className="flex flex-col">
+              <div className="flex border-b-2 border-[#b9b7b79f] ">
+                <span className="text-xs ml-4 py-2 text-center text-[#414040] font-bold uppercase">
+                  Mata Pelajaran
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
+              <div className="flex flex-row border-b-2 border-[#b9b7b79f] py-2 items-center">
+                <Image
+                  src={mapel[0].icon}
+                  width={30}
+                  height={30}
+                  alt="Radix UI"
+                  className="ml-4 cursor-pointer"
+                />
+                <span className="text-xs text-[#414040] font-medium ml-2">
+                  {mapel[0].mapel}
+                </span>
+              </div>
             </div>
           </div>
 
@@ -179,4 +236,4 @@ const PelajaranMenu = ({ mapel }) => (
   )
 );
 
-export default PelajaranMenu;
+export default HoveringCard;
